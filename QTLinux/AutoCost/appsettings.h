048aa38f9011d3836e2ac1b5bdab0fd456097ebd
CVelthoven <cjv.velthoven@gmail.com>
2026-03-03 17:26:07 +0100

    Added skeleton methods to appsettings

--- QTLinux/AutoCost/appsettings.h
+++ QTLinux/AutoCost/appsettings.h
@@ -12,6 +12,7 @@
 //
 //---------------------------------------------------------------------------------------
 #include <QObject>
+#include <QString>
 
 //---------------------------------------------------------------------------------------
 //
@@ -22,6 +23,24 @@ class AppSettings
 {
 public:
     AppSettings();
+
+    //-----------------------------------------------------------------------------------
+    //
+    //  Public methods
+    //
+    //-----------------------------------------------------------------------------------
+    int GetAppSettings(const QString &strKeySection,
+                       const QString &strKeyName,
+                       QString &strKeyValue);
+    int LoadKeyValues();
+    int SetApplicationDomain(const QString &strApplicationDomain);
+    int SetApplicationName(const QString &strApplicationName);
+    int SetApplicationOrganization(const QString &strApplicationOrganization);
+    int SetAppSettings(const QString &strKeySection,
+                       const QString &strKeyName,
+                       QString &strKeyValue);
+    int WriteKeyValues();
+
 };
 
 #endif // APPSETTINGS_H
