6475c674571f7d95b6586b2e9ad15842feb599ff
CVelthoven <cjv.velthoven@gmail.com>
2026-04-13 15:42:20 +0200

    Modify the test values in the overview

--- QTLinux/AutoCost/detailcosttablemodel.cpp
+++ QTLinux/AutoCost/detailcosttablemodel.cpp
@@ -13,6 +13,7 @@
 #include "detailcosttablemodel.h"
 
 #include <QAbstractTableModel>
+#include <QTableView>
 #include <QString>
 
 #include <QDebug>
@@ -24,27 +25,7 @@
 DetailCostTableModel::DetailCostTableModel(QObject *parent)
     : QAbstractTableModel(parent)
 {
-
-    double
-        dTotalCost;
-    int
-        iRecId = -1,
-        iRecordType = -1,
-        iFrequency = -1;
-
-    QString
-        strDate = "",
-        strDescription = "";
-
     DetailedCostSqlTable = new DetailCostSqlModel();
-    int iNBRows = DetailedCostSqlTable->iNbRows;
-    qDebug() << "number of records found: " << iNBRows;
-
-    // for (int iCnt1 = 0; iCnt1 < iNBRows; iCnt1++)
-    // {
-    //     DetailedCostSqlTable->GetRecordData(iCnt1, &iRecId, &iRecordType, &strDate, &strDescription, &dTotalCost, &iFrequency);
-    //     qDebug() << iRecId;
-    // }
 }
 
 //---------------------------------------------------------------------------------------
@@ -64,6 +45,50 @@ DetailCostTableModel::~DetailCostTableModel()
 //  Class methodes
 //
 //---------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------
+//
+//  ConvertSqlrecordToTableViewRow
+//
+//  Input:
+//  - iRowNumber
+//
+//---------------------------------------------------------------------------------------
+void DetailCostTableModel::ConvertSqlrecordToTableViewRow(int iRowNumber)
+{
+    //-----------------------------------------------------------------------------------
+    //
+    //  Local variables
+    //
+    //-----------------------------------------------------------------------------------
+    double
+        dTotalCost;
+    int
+        iRecId = -1,
+        iRecordType = -1,
+        iFrequency = -1;
+
+    QString
+        strDate = "",
+        strDescription = "";
+
+    //-----------------------------------------------------------------------------------
+    //
+    //  Retrieve and convert record to detailtableview layout
+    //
+    //-----------------------------------------------------------------------------------
+    DetailedCostSqlTable->GetRecordData(iRowNumber,
+                                        &iRecId,
+                                        &iRecordType,
+                                        &strDate,
+                                        &strDescription,
+                                        &dTotalCost,
+                                        &iFrequency);
+    strRecID = QString::number(iRecId);
+    qDebug() << strRecID;
+
+}
+
+
 //---------------------------------------------------------------------------------------
 //
 //  rowCount
@@ -95,15 +120,91 @@ int DetailCostTableModel::columnCount(const QModelIndex & /*parent*/) const
 //
 //  data
 //
-//  Required methods for QAbstractTableModel
+//  Fills the fields of the table overview
 //
 //---------------------------------------------------------------------------------------
 QVariant DetailCostTableModel::data(const QModelIndex &index, int role) const
 {
+
     if (role == Qt::DisplayRole)
+    {
+        if (index.row() == CostOverViewRecID)
+        {
+            QString strFieldText = strRecID;
+
+        }
+        else
+        {
         return QString("Row%1, Column%2")
             .arg(index.row() + 1)
             .arg(index.column() +1);
+        }
+    }
+    return QVariant();
+}
 
+bool DetailCostTableModel::setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole)
+{
+    return true;
+}
+
+
+
+//---------------------------------------------------------------------------------------
+//
+//  headerData
+//
+//  Set the headers of the columns
+//
+//---------------------------------------------------------------------------------------
+QVariant DetailCostTableModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role == Qt::DisplayRole && orientation == Qt::Horizontal) {
+        switch (section) {
+        case CostOverViewRecID:
+            return QString("RecID");
+        case CostOverViewRecType:
+            return QString("Type");
+        case CostOverViewDate:
+            return QString("Date");
+        case CostOverViewDescription:
+            return QString("Description");
+        case CostOverViewPeriodic:
+            return QString("Periodic");
+        case CostOverViewElectricity:
+            return QString("Electricity");
+        case CostOverViewOther:
+            return QString("Other");
+        case CostOverViewAccessory:
+            return QString("Accessory");
+        case CostOverViewMillage:
+            return QString("Millage");
+        case CostOverViewMillageTrip:
+            return QString("Km Trip");
+        case CostOverViewKWhTrip:
+            return QString("kWh Trip");
+        case CostOverViewKWhLoaded:
+            return QString("kWh Loaded");
+        case CostOverViewCostKWhperKM:
+            return QString("Cost kWh/km");
+        case CostOverViewAvgEuroPerKWh:
+            return QString("Avg Euro/kWh");
+        case CostOverViewKWhPerPercentage:
+            return QString("kWh/%");
+        case CostOverViewKMPerPercentage:
+            return QString("km/%");
+        case CostOverViewAccuStartPercentage:
+            return QString("Accu Start");
+        case CostOverViewAccuEndPercentage:
+            return QString("Accu End");
+        case CostOverViewAccuUsagePercentage:
+            return QString("Accu Usage");
+        case CostOverViewAccuLoadDeltaPercentage:
+            return QString("Load Delta");
+        case CostOverViewPeriod:
+            return QString("Period");
+
+        }
+    }
     return QVariant();
 }
--- QTLinux/AutoCost/detailcosttablemodel.h
+++ QTLinux/AutoCost/detailcosttablemodel.h
@@ -17,7 +17,9 @@
 #include "detailcostsqlmodel.h"
 
 #include <QAbstractTableModel>
+#include <QTableView>
 #include <QObject>
+#include <QString>
 
 //---------------------------------------------------------------------------------------
 //
@@ -31,12 +33,54 @@ public:
     DetailCostTableModel(QObject *parent = nullptr);
     ~DetailCostTableModel();
 
+    //-----------------------------------------------------------------------------------
+    //
+    //  Default methodes used by TableView
+    //
+    //-----------------------------------------------------------------------------------
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role) override;
 
 private:
+    //-----------------------------------------------------------------------------------
+    //
+    //  Private methodes
+    //
+    //-----------------------------------------------------------------------------------
+    void ConvertSqlrecordToTableViewRow(int iRowNumber);
+
+    //-----------------------------------------------------------------------------------
+    //
+    //  Private variables
+    //
+    //-----------------------------------------------------------------------------------
     DetailCostSqlModel *DetailedCostSqlTable;
+
+    QString
+        strRecID = "",
+        strRecType = "",
+        strRecDate = "",
+        strRecDescription = "",
+        strRecPeriodic ="",
+        strRecElectricity = "",
+        strRecOther = "",
+        strRecAccessory = "",
+        strRecMillage = "",
+        strRecMillageTrip = "",
+        strRecKWhTrip = "",
+        strRecKWhLoaded = "",
+        strRecCostKWhPerKm = "",
+        strRecCostKWhperKM = "",
+        strRecAvgEuroPerKWh = "",
+        strKWhPercentage = "",
+        strKmPerPercentage = "",
+        strAccuStartPercentage = "",
+        strAccuEndPercentage = "",
+        strAccuLoadDeltaPercentange = "",
+        strPeriod = "";
 
     //-----------------------------------------------------------------------------------
     //
