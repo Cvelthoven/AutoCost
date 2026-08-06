2b24ef6d93cac858d2d070b0c2d49be317408aab
CVelthoven <cjv.velthoven@gmail.com>
2026-08-06 14:30:31 +0200

    Cleanup PostGreSQLDB

--- QTLinux/AutoCost/postgresqldb.h
+++ QTLinux/AutoCost/postgresqldb.h
@@ -62,6 +62,7 @@ public:
     //-----------------------------------------------------------------------------------
     //
     //  Status and information
+    //
     bool isOpen() const;
     QSqlDatabase database() const;
     QSqlError lastError() const;
@@ -72,7 +73,6 @@ private:
     //
     //  Private constructor and destructor
     //
-    //-----------------------------------------------------------------------------------
     explicit PostGreSQLDB(QObject* parent = nullptr);
     ~PostGreSQLDB();
 
@@ -82,7 +82,6 @@ private:
     //
     //  Private variables
     //
-    //-----------------------------------------------------------------------------------
     int
         m_iDBServerPort = 0;
 
@@ -96,88 +95,5 @@ private:
     QSqlDatabase
         m_Database;
 };
-
-//---------------------------------------------------------------------------------------
-//
-//  Old code before redesign
-//
-//---------------------------------------------------------------------------------------
-// #include <QSqlDatabase>
-// #include <QtSql>
-// #include <QSqlQueryModel>
-// #include <QString>
-// //#include <QStringList>
-
-// //---------------------------------------------------------------------------------------
-// //
-// //  Class PostGreSQLDB definitions
-// //
-// //---------------------------------------------------------------------------------------
-// class PostGreSQLDB : public QSqlQueryModel
-// {
-//     Q_OBJECT
-// public:
-//     //    PostGreSQLDB(QObject *parent = nullptr);
-//     //    explicit PostGreSQLDB(AppSettings *Application);// needs to be removed
-//     explicit PostGreSQLDB(AppConfiguration *appSettings);
-//     ~PostGreSQLDB();
-
-
-
-//     //-----------------------------------------------------------------------------------
-//     //
-//     //  Setters
-//     //
-//     //-----------------------------------------------------------------------------------
-//     void setStrConnectionName(const QString &newStrConnectionName);
-//     // void setStrDBServerIP(const QString &newStrDBServerIP);
-//     // void setIDBServerPort(int newIDBServerPort);
-//     // void setStrDBName(const QString &newStrDBName);
-//     // void setStrDBUserID(const QString &newStrDBUserID);
-//     // void setStrDBPassword(const QString &newStrDBPassword);
-
-//     //-----------------------------------------------------------------------------------
-//     //
-//     //  Other public methods
-//     //
-//     //-----------------------------------------------------------------------------------
-//     int ConnectDatabase();
-//     //    int ExecQuery(QString *strQuery);
-//     //    int SelectQuery(QString *strQuery);
-//     void close();
-
-//     //-----------------------------------------------------------------------------------
-//     //
-//     //  Public variables
-//     //
-//     //-----------------------------------------------------------------------------------
-//     QSqlDatabase dbAppDatabase;
-
-//     //    QStringList stlRecordContent;
-
-// private:
-//     //-----------------------------------------------------------------------------------
-//     //
-//     //  Private variables
-//     //
-//     //-----------------------------------------------------------------------------------
-//     //    int LoadDatabaseSettings();
-
-//     AppConfiguration *ApplicationConfig = nullptr;
-//     //    AppSettings *Application = nullptr;
-
-//     int iDBServerPort = 0;
-
-//     QString
-//         strConnectionName = "",
-//         strDBServerIP = "",
-//         strDBServerPort = "",
-//         strDBName = "",
-//         strDBUserID = "",
-//         strDBPassword = "";
-
-//     QSqlRecord rResult;
-
-// };
 
 #endif // POSTGRESQLDB_H
